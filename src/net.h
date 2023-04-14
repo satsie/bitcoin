689a0cb0adf9e05eb9ded41a95126275c4a13e81
Stacie <staciewaleyko@gmail.com>
2023-04-14 13:39:05 -0400

    net: create a data structure for network message stats
    New data structure to hold the atomic message and byte counts of each
    network message statistic at the most granular level. Uses a 4D array
    that indexes by direction, network type, connection type, and message
    type.
    
    Co-authored-by: Vasil Dimov <vd@FreeBSD.org>

--- src/net.h
+++ src/net.h
@@ -344,6 +344,40 @@ public:
     void prepareForTransport(CSerializedNetMsg& msg, std::vector<unsigned char>& header) const override;
 };
 
+/**
+ * Placeholder for total network traffic. Split by direction, network, connection
+ * type and message type (byte and message counts).
+ */
+class NetStats
+{
+public:
+    struct MsgStat {
+        std::atomic_uint64_t byte_count; //!< Number of bytes transferred.
+        std::atomic_uint64_t msg_count;  //!< Number of messages transferred.
+
+        MsgStat() : byte_count{0}, msg_count{0} {}
+
+        MsgStat(const MsgStat& x) : byte_count{x.byte_count.load()}, msg_count{x.msg_count.load()} {}
+
+        MsgStat(std::atomic_uint64_t x, std::atomic_uint64_t y) : byte_count{x.load()}, msg_count{y.load()} {}
+    };
+
+    enum class Direction { SENT,
+                            RECV };
+
+    /// Number of elements in `Direction`.
+    static constexpr size_t NUM_DIRECTIONS{2};
+
+    using MultiDimensionalStats = std::array<std::array<std::array<std::array<MsgStat,
+                                                                                // add 1 for the other message type
+                                                                                NUM_NET_MESSAGE_TYPES + 1>,
+                                                                    NUM_CONNECTION_TYPES>,
+                                                        NET_MAX>,
+                                                NUM_DIRECTIONS>;
+
+    MultiDimensionalStats m_data;
+};
+
 struct CNodeOptions
 {
     NetPermissionFlags permission_flags = NetPermissionFlags::None;
